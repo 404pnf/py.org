c396da90f1f58104f46c177d81357e04140de250
404 <404@example.com>
2012-07-27 14:52:22 +0800

    update deploy.sh

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,2 +1,2 @@
-rsync -avz html/* wxkj:/var/www/octopress/newpy.org/
-rsync -avz css wxkj:/var/www/octopress/newpy.org/
+rsync -avz --delete html/* wxkj:/var/www/octopress/newpy.org/
+rsync -avz --delete css wxkj:/var/www/octopress/newpy.org/
