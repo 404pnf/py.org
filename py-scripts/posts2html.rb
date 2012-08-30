5a6c4e1e132cc52f5f4ba5bb2704366ff8eda1d3
404 <404@example.com>
2012-08-30 08:37:08 +0800

    add google tracking code to html

--- py-scripts/posts2html.rb
+++ py-scripts/posts2html.rb
@@ -17,7 +17,7 @@ files.each do |fn|
   mytitle = content.split(/\n/)[0].sub(/^# /, '') # 第一行是题目
   newfn = File.basename(fn, '.txt')
   newfn = newfn + '.html'
-  p newfn
+#  p newfn
   html_content = Kramdown::Document.new(content, :auto_ids => false).to_html
   html_header = <<EOF
 <!DOCTYPE html>
@@ -26,6 +26,21 @@ files.each do |fn|
     <meta charset="utf-8">
     <title>#{mytitle}</title>
     <link href="/css/local.css" rel="stylesheet">
+
+    <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-3301463-1']);
+    _gaq.push(['_trackPageview']);
+
+   (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+    </script>
+
     </head>
     <body>
 EOF
