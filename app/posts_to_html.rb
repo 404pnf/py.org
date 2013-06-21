bfc6898e3042a2ab4ddb90a38e03831c5baf7f49
404pnf <404pnf@exmaple.org>
2013-06-21 11:36:57 +0800

    better variable names; keep folder structure of inputfolder when generate html

--- app/posts_to_html.rb
+++ app/posts_to_html.rb
@@ -9,25 +9,44 @@ require 'erubis'
 # convert \.txt files in inputfolder to html files in outputfolder
 
 DEBUG = nil
-input = File.expand_path ARGV[0] # input folder
-output = File.expand_path ARGV[1] # output folder
+input = ARGV[0] || '../posts'
+output = ARGV[1] || '../site'
+VIEW_FOLDER = '../views'
 
 Dir.mkdir output unless File.exist? output
 
-files = Dir.glob("#{input}/**/*.txt") # files是数组，是带有文件路径的文件
+# files是数组，是带有文件路径的文件
+files = Dir.glob("#{input}/**/*.txt") 
 
 files.each do |fn|
-  content = File.read(fn)
-  mytitle = content.split(/\n/)[0].sub(/^# /, '') # 第一行是题目 '# this is a title'
-  newfn = File.basename(fn, '.txt')
-  newfn = newfn + '.html'
-  p newfn if DEBUG
-  article_content = Kramdown::Document.new(content, :auto_ids => false).to_html
+  # 文章全文
+  content_md = File.read(fn)
+  content_html = Kramdown::Document.new(content_md, :auto_ids => false).to_html
+  # 第一行是题目，比如： '# this is a title'
+  title_raw, _ = content_md.split(/\n/)[0]
+  title = if title
+            title_raw.sub(/^# /, '') 
+          else
+            'no title'
+          end
+    
+  path, fn = File.dirname(fn), File.basename(fn)  
+  #p path # "../posts//2012"
+  newpath =  path.sub(input, output)
+  #p newpath
+  newfn = File.basename(fn, '.txt') + '.html'
+  Dir.mkdir newpath unless File.exist? newpath
+  
   # here comes erubis
-  input = File.read('post.eruby.html')
-  eruby = Erubis::Eruby.new(input)    # create Eruby object
-  article_html =  eruby.result(binding())        # get result  
-  # same old file writing
-  p "generating #{output}/#{newfn}"
-  File.open("#{output}/#{newfn}", article_html)
+  tpl = File.read('../views/post.eruby.html')
+  eruby = Erubis::Eruby.new(tpl)
+  article_html =  eruby.result(binding())
+  
+  p "generating #{newpath}/#{newfn}"
+  File.write("#{newpath}/#{newfn}", article_html)
 end
+
+# copy necessary js/css files from views/ to output-html
+# ref: http://www.ruby-doc.org/stdlib-2.0/libdoc/fileutils/rdoc/FileUtils.html#method-c-copy
+FileUtils.cp_r Dir.glob("#{VIEW_FOLDER}/js/*.js"), '../site/js', :verbose => true
+FileUtils.cp_r Dir.glob("#{VIEW_FOLDER}/css/*.css"), '../site/css', :verbose => true
