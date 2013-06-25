b41a974ca8712896cf66912d861ebfdfa9b56eb5
404pnf <404pnf@exmaple.org>
2013-06-25 16:27:31 +0800

    change suffix name of txt to md

--- app/posts_to_html.rb
+++ app/posts_to_html.rb
@@ -16,12 +16,12 @@ VIEW_FOLDER = '../views'
 Dir.mkdir output unless File.exist? output
 
 # files是数组，是带有文件路径的文件
-files = Dir.glob("#{input}/**/*.txt") 
+files = Dir.glob("#{input}/**/*.md") 
 
 files.each do |fn|
   # 文章全文
   content_md = File.read(fn)
-  content_html = Kramdown::Document.new(content_md, :auto_ids => false).to_html
+  content_html = Kramdown::Document.new(content_md).to_html
   # 第一行是题目，比如： '# this is a title'
   title_raw, _ = content_md.split(/\n/)[0]
   title = if title
@@ -34,7 +34,7 @@ files.each do |fn|
   #p path # "../posts//2012"
   newpath =  path.sub(input, output)
   #p newpath
-  newfn = File.basename(fn, '.txt') + '.html'
+  newfn = File.basename(fn, '.md') + '.html'
   Dir.mkdir newpath unless File.exist? newpath
   
   # here comes erubis
@@ -45,10 +45,3 @@ files.each do |fn|
   p "generating #{newpath}/#{newfn}"
   File.write("#{newpath}/#{newfn}", article_html)
 end
-
-# copy necessary js/css files from views/ to output-html
-# ref: http://www.ruby-doc.org/stdlib-2.0/libdoc/fileutils/rdoc/FileUtils.html#method-c-copy
-=begin
-FileUtils.cp_r Dir.glob("#{VIEW_FOLDER}/js/*.js"), '../site/js', :verbose => true
-FileUtils.cp_r Dir.glob("#{VIEW_FOLDER}/css/*.css"), '../site/css', :verbose => true
-=end
